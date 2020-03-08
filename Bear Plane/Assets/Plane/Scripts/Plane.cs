82d90c25d9ebeaaaeea9c827795197a9a2eb6c73
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-03-08 16:16:42 -0500

    Implemented simple plane mechanics

---
+++ Bear Plane/Assets/Plane/Scripts/Plane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Plane : MonoBehaviour
+{
+    public float acceleration;
+    public float maxVelocity;
+    public int armor;
+
+    [HideInInspector] public float currentVelocity;
+    [HideInInspector] public Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public abstract void Move(Vector2 force);
+}
---
+++ Bear Plane/Assets/Plane/Scripts/SimplePlane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SimplePlane : Plane
+{
+    //SIMPLE
+    //private void Update()
+    //{
+    //    Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+    //    transform.position += Movement * thrust * Time.deltaTime;
+    //}
+
+    //ACTUAL THRUST
+    private void Update()
+    {
+        Vector2 Movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Move(Movement);
+        currentVelocity = (Mathf.Abs(rigidbody2D.velocity.x) + Mathf.Abs(rigidbody2D.velocity.y));
+        Debug.Log("rigidbody2D.velocity: " + currentVelocity);
+    }
+
+    public override void Move(Vector2 direction)
+    {
+        if (currentVelocity <= maxVelocity)
+        {
+            rigidbody2D.AddForce(direction * acceleration);
+        }
+    }
+}
