a6ef315f3931a91523658d28cab5e2652f43c1e0
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-06-30 16:51:05 -0500

    menus

---
+++ Bear Plane/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Button _playButton;
+
+    void Start()
+    {
+        _playButton.onClick.AddListener(HandlePlayPressed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HandlePlayPressed();
+        }
+    }
+
+    void HandlePlayPressed()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
---
+++ Bear Plane/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject _container;
+    [SerializeField] Button _pauseButton;
+    [SerializeField] Button _resumeButton;
+    [SerializeField] Button _quitButton;
+
+    void Start()
+    {
+        _pauseButton.onClick.AddListener(HandlePausePressed);
+        _resumeButton.onClick.AddListener(HandleResumePressed);
+        _quitButton.onClick.AddListener(HandleQuitPressed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_container.activeInHierarchy)
+            {
+                HandleResumePressed();
+            }
+            else
+            {
+                HandlePausePressed();
+            }
+        }
+    }
+
+    void HandlePausePressed()
+    {
+        _container.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void HandleResumePressed()
+    {
+        _container.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void HandleQuitPressed()
+    {
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
